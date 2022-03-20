0b5af00d8c414179144ed96fcc502e12b049ade3
Zaida Kodzoeva <zkod777@gmail.com>
2022-03-20 14:48:41 +0000

    Attempt 2 at PIR

--- testing-server/rip.py
+++ testing-server/rip.py
@@ -1,23 +1,15 @@
-int ledPin = 13;  // LED on Pin 13 of Arduino
-int pirPin = 7; // Input for HC-S501(PIR-sensor)
-int pirValue; // Place to store read PIR Value
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3,GPIO.OUT)     #Define pin 3 as an output pin
 
-void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(pirPin, INPUT);
-  digitalWrite(ledPin, LOW);
-  Serial.begin(9600);
-}
+while True:
+GPIO.output(3,1)   #Outputs digital HIGH signal (5V) on pin 3
+time.sleep(1)      #Time delay of 1 second
 
-void loop() {
-    pirValue = digitalRead(pirPin);
-    digitalWrite(ledPin, pirValue);
-    if(pirValue==HIGH){
-      Serial.println("1");
-      pirValue=0;
-    }
-}
-
+GPIO.output(3,0)   #Outputs digital LOW signal (0V) on pin 3
+time.sleep(1)      #Time delay of 1 second
 # import RPi.GPIO as GPIO
 # import time
 #  
