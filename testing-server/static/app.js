494d1f6a4fb0f3a3f41d4216d031546da88da7a7
Zaida Kodzoeva <zkod777@gmail.com>
2022-03-24 13:31:10 +0000

    Scanner works on ios. Voice control only works on macOS. Speech recognition on raspi fixed

--- testing-server/static/app.js
+++ testing-server/static/app.js
@@ -1,11 +1,16 @@
+'use strict';
 const GOOGLE_BOOKS_API_KEY = "AIzaSyAjnuM0ma2Es__HV4ksbDqfNKpfRcdMzJo";
-const listFormatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
 
-try{
+const listFormatter = new Intl.ListFormat("en", {
+  style: "long",
+  type: "conjunction",
+});
+
+try {
   const actionEl = document.querySelector("#action-select");
   const booksList = document.querySelector("#books-list");
 
-  var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
   const html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", {
     fps: 10,
@@ -13,67 +18,79 @@ try{
   });
 
   function displayBooksList() {
-    fetch('./books').then(res => res.json()).then(isbns => {
-      for (let isbn of isbns)  {
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${GOOGLE_BOOKS_API_KEY}`)
-          .then(gbooksRes => gbooksRes.json())
-          .then(gbooksJson => {
-            const info = gbooksJson.items[0].volumeInfo;
-            booksList.innerHTML += `
-              <li>
-                <img src="${info.imageLinks.thumbnail}"/>
-                <h2>${info.title}</h2>
-                <p>${info.description}</p>
-                <p>${listFormatter.format(info.authors)}</p>
-              </li>`
-          })
-      }
-    })
-    .catch((e) => {
-      document.querySelector(".error-el").innerHTML =
-        e.toString() + "\n" + e.stack;
-    });
+    fetch("./books")
+      .then((res) => res.json())
+      .then((isbns) => {
+        document.querySelector(".error-el").innerHTML += isbns + "\n";
+        booksList.innerHTML = "";
 
+        for (let isbn of isbns) {
+          fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${GOOGLE_BOOKS_API_KEY}`
+          )
+            .then((gbooksRes) => gbooksRes.json())
+            .then((gbooksJson) => {
+              const info = gbooksJson.items[0].volumeInfo;
+              booksList.innerHTML += `
+            <li>
+              <img src="${info.imageLinks.thumbnail}"/>
+              <h2>${info.title}</h2>
+              <p>${info.description}</p>
+              <p>${listFormatter.format(info.authors)}</p>
+            </li>`;
+            })
+            .catch((e) => {
+              document.querySelector(".error-el").innerHTML +=
+                e.toString() + "\n" + e.stack;
+            });
+        }
+      })
+      .catch((e) => {
+        document.querySelector(".error-el").innerHTML +=
+          e.toString() + "\n" + e.stack;
+      });
   }
 
   function onScanSuccess(decodedText, decodedResult) {
     const isbn = decodedText;
-    fetch(`./books?isbn=${isbn}`, {method: actionEl.value === 'add' ? 'PUT' : 'DELETE'})
-      .then(() => {
-        displayBooksList();
-        html5QrcodeScanner.clear();
-        html5QrcodeScanner.render(onScanSuccess);
-      });
+    fetch(`./books?isbn=${isbn}`, {
+      method: actionEl.value === "add" ? "PUT" : "DELETE",
+    }).then(() => {
+      displayBooksList();
+      html5QrcodeScanner.clear();
+      html5QrcodeScanner.render(onScanSuccess);
+    });
   }
 
-  if (!isSafari){
+  if (!isSafari) {
     const grammar = `
-  #JSGF V1.0;
+    #JSGF V1.0;
+    
+    grammar vxmlgram;
+    
+    public <command> =
+        titles
+        | play chill
+        | play intense
+        | rainbow
+        | matrix
+        | remix;
+    `;
 
-  grammar vxmlgram;
+    var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+    var SpeechGrammarList =
+      window.SpeechGrammarList || window.webkitSpeechGrammarList;
+    var SpeechRecognitionEvent =
+      window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
-  public <command> =
-      titles
-      | play chill
-      | play intense
-      | rainbow
-      | matrix
-      | remix;
-  `;
-
-  var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-  var SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
-  var SpeechRecognitionEvent =
-  window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-  const reco = new SpeechRecognition();
-  let speech = new SpeechSynthesisUtterance();
-
-  if (SpeechGrammarList) {
-    const recoList = new SpeechGrammarList();
-    recoList.addFromString(grammar, 1);
-    reco.grammars = recoList;
+    const reco = new SpeechRecognition();
+    let speech = new SpeechSynthesisUtterance();
+    if (SpeechGrammarList) {
+      const recoList = new SpeechGrammarList();
+      recoList.addFromString(grammar, 1);
+      reco.grammars = recoList;
     }
+
     reco.continuous = false;
     reco.lang = "en-US";
     reco.interimResults = false;
@@ -92,17 +109,16 @@ try{
 
     reco.addEventListener("result", (e) => {
       const said = e.results[0][0].transcript;
-      fetch(`./voice-command?command=${encodeURIComponent(said)}`)
-      console.log('we think you said: ', said)
+      fetch(`./voice-command?command=${encodeURIComponent(said)}`);
+      console.log("we think you said: ", said);
       speech.text = said;
       window.speechSynthesis.speak(speech);
     });
   }
-  
-  
 
   displayBooksList();
   html5QrcodeScanner.render(onScanSuccess);
-} catch (e){
-  document.querySelector(".error-el").innerHTML = e.toString() + e.stack;
-  }
+} catch (e) {
+  document.querySelector(".error-el").innerHTML +=
+    e.toString() + "\n" + e.stack;
+}
